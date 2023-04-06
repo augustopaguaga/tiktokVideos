5defb8aaa20896b1765e9855d75f30657f4c3579
Augusto Paguaga <augustopaguaga010@live.com.ar>
2023-04-05 22:54:45 -0300

    tiktok

---
+++ src/components/useItems.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+export default function useItems(data){
+    const [items,setItems] = useState([]);
+
+    useEffect( () => {
+        if(Array.isArray(data)){
+            setItems([...items,...data])
+        }
+    }, [data])
+
+    return items;
+
+}
